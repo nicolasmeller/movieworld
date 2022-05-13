955eeeb76ced72daab2c531c0f57cfdc7429cbbc
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2022-05-13 10:35:53 +0200

    controller fix

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -21,20 +21,20 @@ class MovieController extends Controller
     {
         $key = config('app.movieapi');
         $href = config('app.movieapihref');
-        $collection = Http::get("$href/upcoming?api_key=21f9ad3cc0b8830d984a057a3fc05fb8&language=en-US&page=1") -> json();
+        $collection = Http::get("$href/upcoming?api_key=$key&language=en-US&page=1") -> json();
         return view("pages.UpComing", ['collection' => $collection['results']]);
     }
     public function IndexTopRated(){
         $key = config('app.movieapi');
         $href = config('app.movieapihref');
-        $collection = Http::get("$href/top_rated?api_key=21f9ad3cc0b8830d984a057a3fc05fb8&language=en-US&page=1")-> json();
+        $collection = Http::get("$href/top_rated?api_key=$key&language=en-US&page=1")-> json();
         return view("pages.toprated", ['collection' => $collection['results']]);
     }
     public function show($id){
         $key = config('app.movieapi');
         $href = config('app.movieapihref');
-        $collection = Http::get("$href/$id?api_key=21f9ad3cc0b8830d984a057a3fc05fb8&language=en-US&page=1%22") -> json();
-        $similar = Http::get("$href/$id/similar?api_key=21f9ad3cc0b8830d984a057a3fc05fb8&language=en-US&page=1") -> json();
+        $collection = Http::get("$href/$id?api_key=$key&language=en-US&page=1%22") -> json();
+        $similar = Http::get("$href/$id/similar?api_key=$key&language=en-US&page=1") -> json();
         return view("pages.show", ['collection' => $collection], ['similar' => $similar['results']]);
     }
     public function Index(){
